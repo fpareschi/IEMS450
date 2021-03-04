4dff0cf8d47634b3abefef502ee90705ce6d4ec2
fpareschi <franciscopareschi2023@u.northwestern.edu>
2021-03-04 08:02:32 -0600

    working computation of directio and alpha

--- H4/HW4_EX1617.py
+++ H4/HW4_EX1617.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 from scipy.linalg import ldl
 from scipy.linalg import lu
 
+
 def f(x_1, x_2):
     return - 6*x_1 - 4*x_2 + 13 + x_1**2 + x_2**2
+
+
+def delta_f(x_1, x_2):
+    return np.array([-6 + 2*x_1, -4 + 2*x_2])
 
 delta = 0.01
 x1 = np.arange(-3, 4, delta)
@@ -29,26 +34,56 @@ x_0 = np.array([2, 0])
 
 G = 2*np.identity(2)
 Afull = np.array([[-1, -1], [1, 0], [0,1]])
-RHSfull = np.array([6,4,-3, 0, 0])
+b = np.array([-3, 0, 0])
+#RHSfull = np.array([6,4,-3, 0, 0])
 
 active =[1,2]
 
 
-def compute_p(active_constraint):
+def compute_p(active_constraint, x_k):
     A = Afull[active_constraint, :]
+
+    g_k = delta_f(x_k[0], x_k[1])
+    RHS = np.array([*g_k, *np.zeros(len(active_constraint))])
 
     K_u = np.hstack([G, A.T])
     K_l = np.hstack([A, np.zeros((A.shape[0], A.shape[0]))])
-
     K = np.vstack([K_u, K_l])
-    RHS = np.array([*RHSfull[:2], *RHSfull[2:][active_constraint]])
 
     # Perfom LU factorization and solve system
     P, L, U = lu(K, permute_l=False)
 
     btilde = P @ RHS
     z = np.linalg.solve(L, btilde)
-    x = np.linalg.solve(U, z)
+    p = np.linalg.solve(U, z)
 
-    return x
+    return -p[:2]
 
+
+def compute_lambda(active_constraint, x_k):
+
+    if len(active_constraint)>1:
+        A = Afull[active_constraint, :]
+        Lambda = np.linalg.solve(A, delta_f(x_k[0], x_k[1]))
+        print('la')
+    else:
+        A = Afull[active_constraint, :].flatten()
+        if A[0] !=0:
+            Lambda = delta_f(x_k[0], x_k[1])[0]/A[0]
+        else:
+            Lambda = delta_f(x_k[0], x_k[1])[1] / A[1]
+
+    return Lambda
+
+def compute_alpha(active_constraints, x_k, p_k):
+    not_active = np.setdiff1d([0,1,2],active_constraints)
+    print(not_active)
+
+    alpha_pot = []
+    for i in not_active:
+        if Afull[i, :].T @ p_k<0:
+            alpha_pot.append((b[i] - Afull[i, :].T @ x_k)/(Afull[i,:].T @ p_k))
+
+    alpha_pot = min(alpha_pot)
+    alpha = min(alpha_pot, 1)
+    return alpha
